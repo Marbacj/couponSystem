382fb29178a9273902f85feb40f22a33a36733a9
Marbacj <Uncle.YuH@outlook.com>
2024-02-28 14:46:43 +0800

    1

---
+++ imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/annotation/IgnoreResponseAdvice.java
@@ -0,0 +1,16 @@
+package com.imooc.coupon.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <h1>忽略统一响应注解定义</h1>
+ * Created by Marbach
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+//运行时起作用
+public @interface IgnoreResponseAdvice {
+}
